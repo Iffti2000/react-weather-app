4fe4738a2dc09c2da5ad0349d83a573f3f194e6b
unknown <iftirahman335@gmail.com>
2025-06-03 01:02:52 +0600

    I had made a great mess

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import Weather from './components/Weather'
 
@@ -7,18 +6,6 @@ const App = () => {
     <div className='app'>
        <Weather/>
     </div>
-=======
-import Sidebar from './components/Sidebar/Sidebar'
-import Main from './components/Main/Main'
-
-const App = () => {
-  return (
-    <>
-     <Sidebar/>
-     <Main/>
-    </>
-  
->>>>>>> a081d3784a6911155246728af54900097ca0fde5
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { StrictMode } from 'react'
-<<<<<<< HEAD
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -8,15 +7,4 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-=======
-import {createRoot} from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import ContextProvider from './context/Context.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <ContextProvider>
-    <App />
-  </ContextProvider>,
->>>>>>> a081d3784a6911155246728af54900097ca0fde5
 )
